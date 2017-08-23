d75dfc1533153104374966f43872801aba33b57c
ArpanSriv <arpansri98@gmail.com>
2017-08-23 10:41:10 +0530

    Add files via upload

--- Lift3/main.cpp
+++ Lift3/main.cpp
@@ -6,6 +6,14 @@ using namespace std;
 #define MIN_FLOOR 0
 #define MAX_FLOORS 5
 
+#ifdef _win64
+#define sleepCommand "timeout 3"
+#endif
+
+#ifdef __linux
+#define sleepCommand "sleep 1"
+#endif
+
 enum movement {
         STOP,
         UP, 
@@ -40,6 +48,23 @@ public:
         
     }
     
+    void showLiftStatus() {
+        
+        system("clear");
+       
+        int i = 0;
+        
+        while (i <= this->getCurrentFloor()) {
+        
+            cout << " " ;
+            i++;
+            
+        }
+        
+        cout << this->getCurrentFloor() << endl;
+        
+    }
+    
     int getCurrentFloor() { return mCurrentFloor; }
     
     void setCurrentFloor(int currentFloor) { mCurrentFloor = currentFloor; }
@@ -55,8 +80,8 @@ public:
             while(mTargetFloor != mCurrentFloor) {
              
                 setCurrentFloor(mCurrentFloor = mCurrentFloor + 1);
-                system("sleep 0.5");
-                cout << "Current Floor: " << mCurrentFloor << endl;
+                system(sleepCommand);
+                showLiftStatus();
                 
             }
             
@@ -71,8 +96,8 @@ public:
             while(mTargetFloor != mCurrentFloor) {
              
                 setCurrentFloor(mCurrentFloor = mCurrentFloor - 1);
-                system("sleep 0.5");
-                cout << "Current Floor: " << mCurrentFloor << endl;
+                system(sleepCommand);
+                showLiftStatus();
             }
             
             cout << "REQUEST COMPLETE" << endl;
@@ -122,20 +147,64 @@ void lol(Lift lift) {
 
 int main() {
     
+    int userChoice;
+    char finalChoice;
+    
     Lift lift(MIN_FLOOR, MAX_FLOORS, 0);
     
     cout << "Lift with the properties created: " << endl << "Min Floor: " << lift.getMinFloor() << endl << "Max Floor: " << lift.getMaxFloor() << endl << "Current Floor: " << lift.getCurrentFloor() << endl;
+  
+    do {
+        
+        system("clear");
     
-   thread t1(testMovement, ref(lift));
+        cout << "What do you want to do with the lift?" << endl;
+        cout << "1. Move the lift to desired floor" << endl;
+        cout << "2. Halt the lift in between" << endl;
+        cout << "3. Enter a set of floors to simulate the movement" << endl;
+        cout << "Enter your choice: " ;
+        
+        cin >> userChoice;
+        
+        switch(userChoice) {
+            
+            case 1:{
+                
+                int floor = MIN_FLOOR;
+                
+                do {
+                    
+                    if(floor > MAX_FLOORS) cout << "Bummer! Please enter a valid value between " << MIN_FLOOR << " and " << MAX_FLOORS << "!" << endl;
+                    
+                    cout << "Enter the floor to which you want to move the lift on: ";
+                    cin >> floor;
+                
+                    
+                } while (floor > MAX_FLOORS);
+                    
+                cout << "Starting the lift to move to the floor " << floor << endl;
+                lift.moveLift(floor);
+                
+                if (lift.getCurrentFloor() == floor) cout << "Lift moved to floor " << floor << " successfully " << endl;
+                
+                break;
+            }
+                
+            
+        }
+        
+        cout << "Do you want to perform any other operation?[Y/N]: " ;
+        cin >> finalChoice;
+        
+    } while (finalChoice == 'y' || finalChoice == 'Y');
+        
+  // thread t1(testMovement, ref(lift));
+   ///thread t2(AskUser, ref(lift));
    
-   
+  // t1.join();
+   //t2.join();
     
-   thread t2(AskUser, ref(lift));
-   
-   t1.join();
-   t2.join();
-    
-    cout << lift.getCurrentFloor() << endl;
+   // cout << lift.getCurrentFloor() << endl;
     
     
    
